ae21b8bfb846e7a7680ab791586102cb5cceb2e1
KOZY <kozy@kozy.kozy>
2024-09-19 21:45:27 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+int main()
+{
+    printf("RT Operator, here I come\n");
+    std::cin.get();
+}
